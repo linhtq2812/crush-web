75630fc7d0ad8906601da6bcb8bd89489573edb7
linh.taquang <quanglinh256789@gmail.com>
2025-02-14 19:30:02 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+  introTitle: "Babe à!",
+  introDesc: `Mong em chân cứng đá mềm, đường xa không mỏi, đường đời có anh ! 😚💙`,
+  btnIntro: "hê hê",
+  title: "Làm người iuu anh nhé babe 😙",
+  desc: "Làm sao anh có thể biết cảm giác em lúc này",
+  btnYes: "Vẫn cứ là thích anh <33",
+  btnNo: "Không, Anh trai à :3",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi cho anh (con rể mẹ H)",
+  reply: "Vì anh ngonnnnnn",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Chúc mừng em đã có được anh <3",
+  btnAccept: "Okiiiii lun <3",
+  messLink: "https://facebook.com/quanglinh2812", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
